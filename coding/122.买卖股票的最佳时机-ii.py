d94d79eeaef4dc3adf7af3229d2016c1cf72412b
TSkylsh <30424491+TSkylsh@users.noreply.github.com>
2024-12-19 10:08:38 +0800

    12.18

--- coding/122.买卖股票的最佳时机-ii.py
+++ coding/122.买卖股票的最佳时机-ii.py
@@ -7,6 +7,11 @@
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+            dp=[[0]*len(prices) for _ in range(2)]
+            dp[0][0]=-prices[0]
+            for i in range(1,len(prices)):
+                  dp[0][i] = max(dp[0][i-1],dp[1][i-1]-prices[i])
+                  dp[1][i] = max(dp[1][i-1],dp[0][i-1]+prices[i])
+            return dp[1][len(prices)-1]        
 # @lc code=end
 
