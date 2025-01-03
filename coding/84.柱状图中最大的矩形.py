55b034d84d347b330deb9e76359ba4c4c717f00b
TSkylsh <30424491+TSkylsh@users.noreply.github.com>
2025-01-03 17:09:23 +0800

    1.3

--- coding/84.柱状图中最大的矩形.py
+++ coding/84.柱状图中最大的矩形.py
@@ -7,12 +7,24 @@
 # @lc code=start
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        mp=[]
-        temp=heights[:]
-        temp=list(reversed(temp))
-        temp.append(0)
-        temp=list(reversed(temp))
-        temp.append(1)
-        print(temp)
+        res=0
+        dp=[[0]*len(heights) for _ in range(2)]
+        dp[0][0]=-1
+        for i in range(1,len(heights)):
+            t=i-1
+            while(t>0 and heights[i]<=heights[t]):
+                t=dp[0][t]
+            dp[0][i]=t
+        
+        dp[1][len(heights)-1]=len(heights)
+        for i in range(len(heights)-2,-1,-1):
+            t=i+1
+            while(t<len(heights) and heights[i]<=heights[t]):
+                t=dp[1][t]
+            dp[1][i]=t
+        res=0
+        for i in range(len(heights)):
+            res=max(res,heights[i]*( dp[1][i]-dp[0][i]-1))
+        return res
 # @lc code=end
 
